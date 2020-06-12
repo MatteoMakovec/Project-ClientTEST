daee22b257a1278b17746204849e1eb406200b75
MatteoMakovec <A!AScorpio98>
2020-06-12 11:30:36 +0200

    Perfect communication with Server

--- src/Main.java
+++ src/Main.java
@@ -15,21 +15,21 @@ public class Main {
         }
 
         Socket socket1 = new Socket(serverInetAddress, port);
-        Socket socket2 = new Socket(serverInetAddress, port);
-        Socket socket3 = new Socket(serverInetAddress, port);
-        Socket socket4 = new Socket(serverInetAddress, port);
-        Socket socket5 = new Socket(serverInetAddress, port);
+        //Socket socket2 = new Socket(serverInetAddress, port);
+        //Socket socket3 = new Socket(serverInetAddress, port);
+        //Socket socket4 = new Socket(serverInetAddress, port);
+        //Socket socket5 = new Socket(serverInetAddress, port);
 
         ThreadClient threadClient1 = new ThreadClient(socket1);
-        ThreadClient threadClient2 = new ThreadClient(socket2);
-        ThreadClient threadClient3 = new ThreadClient(socket3);
-        ThreadClient threadClient4 = new ThreadClient(socket4);
-        ThreadClient threadClient5 = new ThreadClient(socket5);
+        //ThreadClient threadClient2 = new ThreadClient(socket2);
+        //ThreadClient threadClient3 = new ThreadClient(socket3);
+        //ThreadClient threadClient4 = new ThreadClient(socket4);
+        //ThreadClient threadClient5 = new ThreadClient(socket5);
 
         threadClient1.start();
-        threadClient2.start();
-        threadClient3.start();
-        threadClient4.start();
-        threadClient5.start();
+        //threadClient2.start();
+        //threadClient3.start();
+        //threadClient4.start();
+        //threadClient5.start();
     }
 }
--- src/ThreadClient.java
+++ src/ThreadClient.java
@@ -10,18 +10,19 @@ public class ThreadClient extends Thread {
 
     public void run() {
         try {
+            BufferedReader brInput = new BufferedReader(new InputStreamReader(System.in));
             BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-            for (int i = 0; i < 5; i++) {
-                String sent = String.format("Hello world n. %d!", i);
-                bw.write(sent + System.lineSeparator());
+            while(true){
+                String toSend = brInput.readLine();
+                bw.write(toSend + System.lineSeparator());
                 bw.flush();
                 String received = br.readLine();
-                System.out.printf("Sent: %s%nReceived: %s%n", sent, received);
+                System.out.printf("Sent: %s%nReceived: %s\n\n", toSend, received);
+                if(socket.isClosed() || received == null) break;
             }
-            bw.write("bye" + System.lineSeparator());
-            bw.flush();
+
         } catch (IOException e) {
             System.err.printf("IO error: %s\n", e);
         } finally {
